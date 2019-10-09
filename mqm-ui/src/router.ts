210b793b52c1d3b07354df196bc0f1889708be63
Álvaro Brey <alvaro.brv@gmail.com>
2019-10-09 20:41:06 +0200

    ui: Add a TODO for router, and fix vuetify compilation with TS

--- mqm-ui/src/router.ts
+++ mqm-ui/src/router.ts
@@ -3,7 +3,7 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 
 Vue.use(Router);
-
+// todo add catch-all route for errors (https://router.vuejs.org/guide/essentials/history-mode.html#caveat)
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
