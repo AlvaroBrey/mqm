030da628625e4c2858d3cee7a53c1c1cf96e7cde
Álvaro Brey <alvaro.brv@gmail.com>
2019-10-09 21:33:21 +0200

    Add mongo container to docker-compose, and configure mqm-api accordingly

--- mqm-api/src/main/kotlin/db/DB.kt
+++ mqm-api/src/main/kotlin/db/DB.kt
@@ -6,6 +6,14 @@ import org.litote.kmongo.KMongo
 object DB {
     //TODO parameterize
     private const val DB_NAME = "mqm"
+    private val HOST = System.getenv("DB_HOST") ?: "mongo"
+    private val PORT = System.getenv("DB_PORT") ?: "27017"
+
     //TODO parameters for mongo initialization
-    val instance: MongoDatabase by lazy { KMongo.createClient().getDatabase(DB_NAME) }
+    val instance: MongoDatabase by lazy {
+        KMongo.createClient(
+                host = HOST,
+                port = PORT.toInt()
+        ).getDatabase(DB_NAME)
+    }
 }
