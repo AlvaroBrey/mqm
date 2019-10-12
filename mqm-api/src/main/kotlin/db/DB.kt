3e3afaeae734088f7b1f4f9a157d19a4a94f240c
Álvaro Brey <alvaro.brv@gmail.com>
2019-10-12 12:46:28 +0200

    api: Remove obsolete TODO

--- mqm-api/src/main/kotlin/db/DB.kt
+++ mqm-api/src/main/kotlin/db/DB.kt
@@ -4,12 +4,10 @@ import com.mongodb.client.MongoDatabase
 import org.litote.kmongo.KMongo
 
 object DB {
-    //TODO parameterize
     private const val DB_NAME = "mqm"
     private val HOST = System.getenv("DB_HOST") ?: "mongo"
     private val PORT = System.getenv("DB_PORT") ?: "27017"
-
-    //TODO parameters for mongo initialization
+    
     val instance: MongoDatabase by lazy {
         KMongo.createClient(
                 host = HOST,
