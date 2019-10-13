7914a1fa5c490aa47b953c205b516bfed4bbcd50
Álvaro Brey <alvaro.brv@gmail.com>
2019-10-13 22:31:41 +0200

    ui: docker: change entrypoint default var to reflect new container names

--- mqm-ui/docker/entrypoint.sh
+++ mqm-ui/docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-export API_HOST=${API_HOST:-mqm-api}
+export API_HOST=${API_HOST:-api}
 export API_PORT=${API_PORT:-8080}
 
 echo "API url: http://${API_HOST}:${API_PORT}"
